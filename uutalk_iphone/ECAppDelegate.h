43848bae3eb9465a03a6fee8ba5cb0ffae769f5b
starkingwx <starkingwx@gmail.com>
2013-05-20 14:13:51 +0800

    Initial Commit

---
+++ uutalk_iphone/ECAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ECAppDelegate.h
+//  uutalk_iphone
+//
+//  Created by king star on 13-5-20.
+//  Copyright (c) 2013年 king star. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ uutalk_iphone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  uutalk_iphone
+//
+//  Created by king star on 13-5-20.
+//  Copyright (c) 2013年 king star. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ECAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ECAppDelegate class]));
+    }
+}
